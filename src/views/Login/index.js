d154630e02bf9621f6873698d159b2670584c2c5
ZiQi <1157988746@qq.com>
2020-05-21 17:07:43 +0800

    登录页面静态完成

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import style from "./index.css";
 
 export default class Login extends Component {
+  NormalLoginForm = () => {
+    const onFinish = (values) => {
+      console.log("Received values of form: ", values);
+    };
+  };
   render() {
     return (
-      <div>
-        login
+      <div className="login_box">
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={this.onFinish}
+        >
+          <div className="login_stu">
+            <p>学员后台系统</p>
+          </div>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-    )
+    );
   }
 }
